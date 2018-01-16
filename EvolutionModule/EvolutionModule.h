263a5ac2aa512577d30b77f120b835b0b96b9c1e
ThomJordan <thomjordan@users.noreply.github.com>
2018-01-16 04:00:43 -0500

    Initial Commit

---
+++ EvolutionModule/EvolutionModule.h
@@ -0,0 +1,19 @@
+//
+//  EvolutionModule.h
+//  EvolutionModule
+//
+//  Created by Thom Jordan on 1/16/18.
+//  Copyright © 2018 Thom Jordan. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for EvolutionModule.
+FOUNDATION_EXPORT double EvolutionModuleVersionNumber;
+
+//! Project version string for EvolutionModule.
+FOUNDATION_EXPORT const unsigned char EvolutionModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EvolutionModule/PublicHeader.h>
+
+
